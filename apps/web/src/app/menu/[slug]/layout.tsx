1a6c3752d8d2dca249f2c13ad904c70df3d559bf
Andres4532 <agcastel75@gmail.com>
2026-08-20 10:47:51 -0400

    cambios realizados

--- apps/web/src/app/menu/[slug]/layout.tsx
+++ apps/web/src/app/menu/[slug]/layout.tsx
@@ -1,14 +1,7 @@
-import { MenuGeolocationPrompt } from '@/components/menu/MenuGeolocationPrompt';
-
 export default function MenuSlugLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <MenuGeolocationPrompt />
-      {children}
-    </>
-  );
+  return children;
 }
